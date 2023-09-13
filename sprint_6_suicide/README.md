89e11430b1d176e0a25833c1c30391578ec796ac
Hollis Krause <126561334+UncreativeUsername37@users.noreply.github.com>
2023-09-12 17:52:23 -0700

    Just adding files

---
+++ sprint_6_suicide/README.md
@@ -0,0 +1,6 @@
+# WHO suicide dataset analysis
+Using the data that can be found at https://www.kaggle.com/andrewmvd/suicide-dataset, after a bit of cleaning, I made a bunch of Plotly scatterplots in an attempt to find which factors matter the most. Python 3.9, pandas, and Plotly is all the notebook uses. Theoretically, a version of this with more factors, and more up to date data, and a type of analysis that cares about interdependent factors more, could be used to decide what kinds of things should get more funding.
+
+Spoiler: the best thing you can do is have more professional psychiatrists.
+
+As of the time of writing, the web app that was made as part of the assignment still works too: https://practicum-s6.onrender.com/. If you want to see the app locally, you can clone this and install Streamlit and streamlit run app.py on whatever Python command line.
---
+++ sprint_6_suicide/app.py
@@ -0,0 +1,74 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+df = pd.read_csv("notebooks/suicide_dataset-11.csv")
+df = df.replace({'Yes': True, 'No': False})
+df2015 = df[df["year"] == 2015].sort_values(by=["country"])
+df2015g = df2015[df2015["sex"] != "Both"]
+df2015b = df2015[df2015["sex"] == "Both"]
+df2016 = df[df["year"] == 2016].sort_values(by=["country"])
+df2016g = df2016[df2016["sex"] != "Both"]
+df2016b = df2016[df2016["sex"] == "Both"]
+
+st.title("What accommodations best prevent suicide?")
+
+# And now we make things with our things
+st.write("The dataset I used covered 2015 and 2016. In the analysis, I focussed on 2016 for being more recent and having more data to work with. The numbers by country weren't very different.")
+yrbutton = st.radio("Year", ("2015", "2016"))
+
+if yrbutton == "2015":
+    world = px.choropleth(df2015b, projection="winkel tripel", locations="iso", color="suicide_rate", color_continuous_scale=px.colors.sequential.Bluered)
+    hist = px.histogram(df2015b, x="suicide_rate")
+
+if yrbutton == "2016":
+    world = px.choropleth(df2016b, projection="winkel tripel", locations="iso", color="suicide_rate", color_continuous_scale=px.colors.sequential.Bluered)
+    hist = px.histogram(df2016b, x="suicide_rate")
+
+st.plotly_chart(world)
+
+st.write("The rate listed is per 100&nbsp;000 people. If it looks like there are more countries with lower rates, it's because there are:")
+st.plotly_chart(hist)
+
+st.write("If you want to compare a specific set of countries more precisely than what looking at slightly different shades of purple allows, go ahead:")
+if yrbutton == "2015":
+    selected = st.multiselect("Country selection", df2015b)
+    filtered = df2015b[df2015b["country"].isin(selected)].sort_values(by=["suicide_rate"], ascending=False)
+if yrbutton == "2016":
+    selected = st.multiselect("Country selection", df2016b)
+    filtered = df2016b[df2016b["country"].isin(selected)].sort_values(by=["suicide_rate"], ascending=False)
+barchart = px.bar(filtered, x="country", y="suicide_rate")
+if filtered.empty == False:
+    st.plotly_chart(barchart)
+
+st.write("I looked at most of the variables the dataset had to offer, and of all the scatterplots I made for 2016, there were two with an R² that was even over .05 when genders were combined, and the one with the bigger one had a positive trend. Here they are. First for the number of mental hospitals per 100 000 people:")
+
+mentalh_g = st.checkbox("Gendered", key="mentalh")
+if mentalh_g:
+    if yrbutton == "2015":
+        mentalh = px.scatter(df2015g[df2015g["mental_hospitals_per_100k"] < 0.5], x="mental_hospitals_per_100k", y="suicide_rate", trendline="ols", color="sex")
+    if yrbutton == "2016":
+        mentalh = px.scatter(df2016g[df2016g["mental_hospitals_per_100k"] < 0.5], x="mental_hospitals_per_100k", y="suicide_rate", trendline="ols", color="sex")
+else:
+    if yrbutton == "2015":
+        mentalh = px.scatter(df2015b[df2015b["mental_hospitals_per_100k"] < 0.5], x="mental_hospitals_per_100k", y="suicide_rate", trendline="ols")
+    if yrbutton == "2016":
+        mentalh = px.scatter(df2016b[df2016b["mental_hospitals_per_100k"] < 0.5], x="mental_hospitals_per_100k", y="suicide_rate", trendline="ols")
+st.plotly_chart(mentalh)
+
+st.write("And here for psychiatrists:")
+
+psyiatr_g = st.checkbox("Gendered", key="psyiatr")
+if psyiatr_g:
+    if yrbutton == "2015":
+        psyiatr = px.scatter(df2015g[df2015g["psychiatrists_per_100k"] < 2], x="psychiatrists_per_100k", y="suicide_rate", trendline="ols", color="sex")
+    if yrbutton == "2016":
+        psyiatr = px.scatter(df2016g[df2016g["psychiatrists_per_100k"] < 2], x="psychiatrists_per_100k", y="suicide_rate", trendline="ols", color="sex")
+else:
+    if yrbutton == "2015":
+        psyiatr = px.scatter(df2015b[df2015b["psychiatrists_per_100k"] < 2], x="psychiatrists_per_100k", y="suicide_rate", trendline="ols")
+    if yrbutton == "2016":
+        psyiatr = px.scatter(df2016b[df2016b["psychiatrists_per_100k"] < 2], x="psychiatrists_per_100k", y="suicide_rate", trendline="ols")
+st.plotly_chart(psyiatr)
+if yrbutton == "2015":
+    st.write("You can see what I mean with the \"not as much data\" thing.")
